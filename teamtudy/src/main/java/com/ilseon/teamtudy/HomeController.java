2c9adc48d68bd6101f2949bd73402c691d60070c
일선 <cis7931@naver.com>
2016-08-30 17:22:29 +0900

    DB연동 확인

--- teamtudy/src/main/java/com/ilseon/teamtudy/HomeController.java
+++ teamtudy/src/main/java/com/ilseon/teamtudy/HomeController.java
@@ -4,6 +4,9 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -16,12 +19,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	// test
+	@Inject
+	private SqlSession sqlSession;
+    private static final String NAMESPACE = "com.ilseon.teamtudy.boardmapper";	
+	// test
+    
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -32,6 +37,10 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		// test
+		System.out.println((int)sqlSession.selectOne(NAMESPACE + ".test"));
+		// test
 		
 		return "home";
 	}
