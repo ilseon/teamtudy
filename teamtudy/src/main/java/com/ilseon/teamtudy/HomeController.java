553cdb91f35d6e189cfcfd57b38c1d1913b374bc
조일선 <cis7931@mobigen.com>
2019-03-29 14:04:55 +0900

    다른라인

--- teamtudy/src/main/java/com/ilseon/teamtudy/HomeController.java
+++ teamtudy/src/main/java/com/ilseon/teamtudy/HomeController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class HomeController { // test
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
     
 	@RequestMapping(value = "/", method = RequestMethod.GET)
