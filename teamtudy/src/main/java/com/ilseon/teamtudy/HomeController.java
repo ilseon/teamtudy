e8e37ebf4e9e5f3f314d483be2f2bf78fa41e297
jungsuda <jungsuda@gmail.com>
2019-03-29 13:44:11 +0900

    커밋 테스트

--- teamtudy/src/main/java/com/ilseon/teamtudy/HomeController.java
+++ teamtudy/src/main/java/com/ilseon/teamtudy/HomeController.java
@@ -31,7 +31,8 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-			
+		
+		System.out.println("test");
 		return "home";
 	}
 	
